2dd66090721dcc0986b14bd61eb55217315afe7d
CatalysisArNisha <arnisha@catalysis.com>
2022-03-01 00:37:01 -0800

    worked on search and regions filters

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -25,6 +25,16 @@ export default function Countries(props) {
         }
     }
 
+    const regionFilter = (e) => {
+        if(e.target.value === ""){
+            console.log(e.target.value)
+            setResults(props.countries);
+        }else{
+            let filteredRegions = props.countries.filter(countries => countries.region.includes(e.target.value));
+            setResults(filteredRegions);
+        }
+    }
+
     return (
         <>
             <div className='search-container row'>
@@ -33,18 +43,14 @@ export default function Countries(props) {
                     <input className='search-input' name="search" placeholder='Search for a country...' onChange={searchFilter} />
                 </div>
                 <div className='region-container col-md-4 d-flex justify-content-end'>
-                <div className="dropdown">
-                    <button className="btn dropdown-toggle" type="button" id="regionDropDown" data-bs-toggle="dropdown" aria-expanded="false">
-                    Filter by Region
-                    </button>
-                        <ul className="dropdown-menu" aria-labelledby="regionDropDown">
-                            {region.map(regions => {
-                                return (
-                                    <li key={regions}><a className="dropdown-item" href="#">{regions}</a></li>
-                                )
-                            })}
-                        </ul>
-                    </div>
+                    <select className='region-dropdown' onChange={regionFilter}>
+                        <option>Filter by Region</option>
+                        {region.map(regions => {
+                            return (
+                                <option key={regions}>{regions}</option>
+                            )
+                        })}
+                    </select>
                 </div>
             </div>
             <div className='countries-container row row-cols-lg-4 row-cols-md-2 gx-5 gy-5'>
--- src/components/Country.js
+++ src/components/Country.js
@@ -6,41 +6,51 @@ export default function Country(props) {
     const {name} = useParams();
 
     const country = props.countries.find(country => country.name === name);
-    localStorage.getItem('data');
     
-  return (
-      <div className='container'>
+    // const alpha3Code = props.countries.map(ac => ac.alpha3Code);
+    // const borderName = props.countries.map(country => {
+    //     if(alpha3Code === country.borders){
+    //         return country.name;
+    //     }
+    // });
+    // console.log(borderName);
+    
+    return (
+        <div className='container'>
         <div className='row'>
             <div className='col'>
-                <Link to="/"><button><HiArrowNarrowLeft /> Back</button></Link>
+                <Link to="/"><button className='back-btn'><HiArrowNarrowLeft size={20}/> Back</button></Link>
             </div>
         </div>
-        <div className='row'>
-            <div className='col-md-6'>
+        <div className='row single-country-container'>
+            <div className='col-md-6 pe-5'>
                 <img src={country.flags.svg} className="img-fluid" alt=""/>
             </div>
-            <div className='col-md-6'>
+            <div className='col-md-6 d-flex flex-column justify-content-center px-5'>
                 <div className='row'>
-                    <div className='country-title col'>
+                    <div className='single-country-title col'>
                         <h2>{country.name}</h2>
                     </div>
                 </div>
                 <div className='row'>
-                    <div className='country-info col-md-6'>
+                    <div className='single-country-info col-md-6'>
                         <div><span>Native Name:</span> {country.name}</div>
                         <div><span>Population:</span> {country.population.toLocaleString()}</div>
                         <div><span>Region:</span> {country.region}</div>
                         <div><span>Sub Region:</span> {country.subregion}</div>
                         <div><span>Capital:</span> {country.capital}</div>
                     </div>
-                    <div className='country-info col-md-6'>
+                    <div className='single-country-info col-md-6'>
                         <div><span>Top Level Domain:</span> {country.topLevelDomain}</div>
                         <div><span>Currencies:</span> {country.currencies.map(currency => currency.name)}</div>
                         <div><span>Languages:</span> {country.Languages}</div>
                     </div>
                 </div>
+                <div className='row'>
+                    <div><span>Border Countries</span> {country.borders.map(border => <Link to={country.name} key={border} className="border-code">{border}</Link>)}</div>
+                </div>
             </div>
         </div>
-      </div>
-  )
+        </div>
+    )
 }
